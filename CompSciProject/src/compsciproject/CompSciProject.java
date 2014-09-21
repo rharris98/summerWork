ea24cb252a2ef756c2a046bf6438b80a96350e02
Ricky <rickyh8198@msn.com>
2014-09-21 14:03:16 -0400

    initial

---
+++ CompSciProject/src/compsciproject/CompSciProject.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compsciproject;
+
+/**
+ *
+ * @author Ricky
+ */
+public class CompSciProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
